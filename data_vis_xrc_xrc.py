8512059554753acb1ed75831af7729f05e63ffc1
Ryan Krauss <ryanlists@gmail.com>
2013-10-07 16:11:39 -0500

    I think something weird happened with xrced here....

--- data_vis_xrc_xrc.py
+++ data_vis_xrc_xrc.py
@@ -16,29 +16,6 @@ def get_resources():
 
 
 
-class xrcmain_dialog(wx.Dialog):
-#!XRCED:begin-block:xrcmain_dialog.PreCreate
-    def PreCreate(self, pre):
-        """ This function is called during the class's initialization.
-        
-        Override it for custom setup before the window is created usually to
-        set additional window styles using SetWindowStyle() and SetExtraStyle().
-        """
-        pass
-        
-#!XRCED:end-block:xrcmain_dialog.PreCreate
-
-    def __init__(self, parent):
-        # Two stage creation (see http://wiki.wxpython.org/index.cgi/TwoStageCreation)
-        pre = wx.PreDialog()
-        self.PreCreate(pre)
-        get_resources().LoadOnDialog(pre, parent, "main_dialog")
-        self.PostCreate(pre)
-
-        # Define variables for the controls, bind event handlers
-
-
-
 
 
 # ------------------------ Resource data ----------------------
@@ -47,4 +24,4 @@ def __init_resources():
     global __res
     __res = xrc.EmptyXmlResource()
 
-    __res.Load('figure_name_dialog.xrc')
+    __res.Load('block_replacement_dialog_xrc.xrc')
